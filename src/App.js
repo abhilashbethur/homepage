a7b9f39bd2a9868ed19113eec5f7a7ffa7dab39b
m4xbilla <abhilashbethur@gmail.com>
2020-11-22 11:18:03 +0530

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello , homepage under construction!
+      Hello , homepage under construction!!
     </div>
   );
 }
